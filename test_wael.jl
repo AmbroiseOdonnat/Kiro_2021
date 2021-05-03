3b7501ff3acc0269e87ea69473a1f389307267d8
Emma Beuze <emma.beuze@eleves.enpc.fr>
2021-05-03 17:01:03 +0200

    blabla

--- test_wael.jl
+++ test_wael.jl
@@ -1,6 +1,17 @@
 println("Coucou Emma et Ambroise j'ai ajouté ce fichier avec Git pour atom")
 println("coucou les ptits bg")
 
+<<<<<<< HEAD
+
+f = x -> x^2
+
+println(f(5))
+=======
 function xcube(x)
     return x**3
+<<<<<<< HEAD
+blabla
+=======
     
+>>>>>>> 09373dd492419c079529baede29cba4c53d76564
+>>>>>>> e5850afe7f3e3cddd29c36d7f8fd2785242234db
