1c565af6c64b39bc131eb7f7e01827e20f221bf7
Emma Beuze <emma.beuze@eleves.enpc.fr>
2021-05-06 15:37:30 +0200

    itineraire correction

--- shakiro/itineraire.jl
+++ shakiro/itineraire.jl
@@ -2,17 +2,17 @@ println("Voies lignes : ", voies_ligne)
 println("Voies quai : ", voies_quai)
 
 
-function itineraire_possible(voie_ligne,voie_quai)  #renvoie true s'il existe un itineraire entre le quai i et le quai j
+function itineraire_possible(voie_ligne,voie_quai,sens)  #renvoie true s'il existe un itineraire entre le quai i et le quai j
     it_possible = []
     n = nb_itineraire()
     for id in 1:n
         sensdep,l,q = caracteristique_itineraire(id-1)
-        if l==voie_ligne && q==voie_quai
+        if sens==sensdep && l==voie_ligne && q==voie_quai
             push!(it_possible,id)
         end
     end
     return it_possible
 end
 
-l = itineraire_possible("VU2_S","11")
+l = itineraire_possible("VU2_S","11",false)
 print(l)
