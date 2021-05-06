9e0a67fb7b7ccd38198d44e66f3a0f9a6bdd6f97
Emma Beuze <emma.beuze@eleves.enpc.fr>
2021-05-06 14:41:22 +0200

    instance

--- shakiro/import_all.jl
+++ shakiro/import_all.jl
@@ -2,4 +2,6 @@
 #include("write.jl")
 include("solution.jl")
 include("Test_Ambroise.jl")
-Pkg.add("JSON")
+include("instance.jl")
+#Pkg.add("JSON")
+import JSON
--- shakiro/instance.jl
+++ shakiro/instance.jl
@@ -1,48 +1,61 @@
-struct Instance
+function lire_instance(path::String)
+    dict = JSON.parsefile(path)
 
-    trains::Vector{Trains}
-    itineraires::Vector{Itineraires}
-    voiesAQuai::Vector{VoiesAQuai}
-    voiesEnLigne::Vector{VoiesEnLigne}
-    interdictionsQuais::Vector{InterdictionsQuais}
-    contraintes::Vector{contraintes}
+    groupes = dict["trains"]
+    itineraires = dict["itineraires"]
+    voies_quai = dict["voiesAQuai"]
+    voies_ligne = dict["voiesEnLigne"]
+    interdictions = dict["interdictionsQuais"]
+    contraintes = dict["contraintes"]
 
-    Instance(; trains, itineraires, voiesAQuai,voiesEnLigne,interdictionQuais,contraintes) =
-        new(trains, itineraires, voiesAQuai,voiesEnLigne,interdictionQuais,contraintes)
+    return groupes, itineraires, voies_quai, voies_ligne, interdictions, contraintes
 end
 
-function Base.show(io::IO, instance::Instance)
-    str = "\nInstance"
-    str *= "\n   Nombre de trains: $(instance.trains)"
-    str *= "\n   Nombre d'itinéraires: $(instance.itineraires)"
-    str *= "\n   Nombre de voies à quai: $(instance.voiesAQuai)"
-    str *= "\n   Nombre de voies en ligne: $(instance.voiesEnLigne)"
-    print(io, str)
-end
 
-function lire_instance(path::String)::Instance
-    data = open(path) do file
-        readlines(file)
+groupes, itineraires, voies_quai, voies_ligne, interdictions, contraintes = lire_instance("C:\\Users\\Emma\\Desktop\\instances\\A.json")
+
+function nombre_trains()
+    nb = 0
+    for groupe in groupes
+        nb += length(groupe)
     end
-
-    dims = lire_dimensions(data[1])
-    emballages = [lire_emballage(data[1+e], dims) for e = 1:dims.E]
-    usines = [lire_usine(data[1+dims.E+u], dims) for u = 1:dims.U]
-    fournisseurs = [lire_fournisseur(data[1+dims.E+dims.U+f], dims) for f = 1:dims.F]
-    graphe = lire_graphe(data[1+dims.E+dims.U+dims.F+1:end], dims)
-
-    return Instance(
-        J = dims.J,
-        U = dims.U,
-        F = dims.F,
-        E = dims.E,
-        L = dims.L,
-        γ = dims.γ,
-        ccam = dims.ccam,
-        cstop = dims.cstop,
-        emballages = emballages,
-        usines = usines,
-        fournisseurs = fournisseurs,
-        graphe = graphe,
-    )
+    return nb
 end
+
+function nb_itineraire()
+    return length(itineraires)
+end
+
+
+function caracteristique_train(id)
+    for groupe in groupes
+        for train in groupe
+            if id == train["id"]
+                sensdep = train["sensDepart"]
+                l = train["voieEnLigne"]
+                q = train["voieAQuai"]
+                type_circul = train["typeCirculation"]
+                date_heure = train["dateHeure"]
+                materiel = train["typesMateriels"]
+                return sensdep,l,q,type_circul,date_heure,materiel
+            end
+        end
+    end
+end
+
+
+caracteristique_train(15)
+
+function caracteristique_itineraire(id)
+    for itineraire in itineraires
+        if id == itineraire["id"]
+            sensdep = itineraire["sensDepart"]
+            l = itineraire["voieEnLigne"]
+            q = itineraire["voieAQuai"]
+            return sensdep,l,q
+        end
+        end
+    end
+end
+
+caracteristique_itineraire(15)
