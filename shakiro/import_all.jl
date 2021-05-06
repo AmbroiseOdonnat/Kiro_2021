9307c9a64853496589f4eac0c889745f709d3f75
AmbroiseOdonnat <ambroise.odonnat@eleves.enpc.fr>
2021-05-06 16:05:05 +0200

    PLNE Ambroise

--- shakiro/import_all.jl
+++ shakiro/import_all.jl
@@ -1,10 +1,6 @@
 #include("instance.jl")
 #include("write.jl")
 include("solution.jl")
-<<<<<<< HEAD
-include("Test_Ambroise.jl")
 include("instance.jl")
-=======
->>>>>>> b8e746e013dda09a4973a4e2acaf4c7d92b0ff7c
 #Pkg.add("JSON")
 import JSON
